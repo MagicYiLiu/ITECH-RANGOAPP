0bdf1750dfdc8a54b0e390e4c3bc714d97c84658
Rick <rick@pc-67-46.customer.ask4.lan>
2020-02-20 15:44:43 +0000

    chapter8 excerise

--- tango_with_django_project/rango/forms.py
+++ tango_with_django_project/rango/forms.py
@@ -1,10 +1,9 @@
 from django import forms
 from rango.models import Page, Category
 
-# We could add these forms to views.py, but it makes sense to split them off into their own file.
-
 class CategoryForm(forms.ModelForm):
-    name = forms.CharField(max_length=Category.NAME_MAX_LENGTH, help_text="Please enter the category name.")
+    name = forms.CharField(max_length=128,
+                           help_text="Please enter the category name.")
     views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
     likes = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
     slug = forms.CharField(widget=forms.HiddenInput(), required=False)
@@ -14,14 +13,15 @@ class CategoryForm(forms.ModelForm):
         fields = ('name',)
 
 class PageForm(forms.ModelForm):
-    title = forms.CharField(max_length=Page.TITLE_MAX_LENGTH, help_text="Please enter the title of the page.")
-    url = forms.URLField(max_length=200, help_text="Please enter the URL of the page.")
+    title = forms.CharField(max_length=128,
+                            help_text="Please enter the title of the page.")
+    url = forms.URLField(max_length=200,
+                         help_text="Please enter the URL of the page.")
     views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
 
     class Meta:
         model = Page
         exclude = ('category',)
-    
     def clean(self):
         cleaned_data = self.cleaned_data
         url = cleaned_data.get('url')
